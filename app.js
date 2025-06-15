29a926cdb33ad81d59dbfa00cbf1babcaa620e9c
banga22 <danilfomin74@gmail.com>
2025-06-16 01:56:33 +0300

    Update and rename script.js to app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8s1_iQ04S5a6rlzOkLbsg-weoCQzzG50",
+  authDomain: "profiles-site.firebaseapp.com",
+  projectId: "profiles-site",
+  storageBucket: "profiles-site.firebasestorage.app",
+  messagingSenderId: "539714982046",
+  appId: "1:539714982046:web:d09296102e765bcfcb2b8f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
